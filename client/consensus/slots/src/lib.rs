09a92c5f1c5f7701a2d89bb936e3a69e548a587e
imstar15 <imstar15@qq.com>
2021-09-07 17:06:50 +0800

    Add logs

--- client/consensus/slots/src/lib.rs
+++ client/consensus/slots/src/lib.rs
@@ -753,11 +753,11 @@ where
 		// head.
 		if *slot_now <= *chain_head_slot + interval {
 			print("debug!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-			print("debug!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-			// info!(
-			// 	target: logging_target,
-			// 	"Backing off claiming new slot for block authorship: finality is lagging.",
-			// );
+			// print("debug!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+			info!(
+				target: logging_target,
+				"Backing off claiming new slot for block authorship: finality is lagging.",
+			);
 			true
 		} else {
 			false
